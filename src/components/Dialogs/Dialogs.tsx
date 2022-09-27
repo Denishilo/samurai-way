6fc6f3bd59423d4a4dbc620479307cd3c1235a7b
Denishilo <denisanishenko@gmail.com>
2022-09-27 23:26:06 +0300

    add styles

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,30 +2,48 @@ import React from "react";
 import styles from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type DialogUserTypeProps = {
+    name: string
+    id:number
+}
+
+type DialogItemTypeProps = {
+    message: string
+}
+
+const DialogUser = (props: DialogUserTypeProps) => {
+    let path = `/dialogs/${props.id}`;
+    return (
+        <div>
+            <div className={styles.dialogs__user}><NavLink to={path}>{props.name}</NavLink></div>
+        </div>
+    )
+}
+
+const DialogItem = (props: DialogItemTypeProps)=>{
+    return (
+            <div className={styles.dialogs__item}>{props.message}</div>
+    )
+}
 
 export const Dialogs = () => {
     return (
         <div className={styles.dialogs__wrapper}>
             <div className={styles.dialogs__users}>
-                <div className={styles.dialogs__item}><NavLink to={'/dialogs/1'}>
-                    Denis
-                </NavLink></div>
-                <div className={styles.dialogs__item}><NavLink to={'/dialogs/2'}>
-                    Kseniya
-                </NavLink></div>
-                <div className={styles.dialogs__item}><NavLink to={'/dialogs/3'}>
-                    Kirill
-                </NavLink></div>
-                <div className={styles.dialogs__item}><NavLink to={'/dialogs/4'}>
-                    Nikita
-                </NavLink></div>
+                <DialogUser name={'Denis'} id={1}/>
+                <DialogUser name={'Kseniya'} id={2}/>
+                <DialogUser name={'Nikita'} id={3}/>
+                <DialogUser name={'Kirill'} id={4}/>
+                <DialogUser name={'Svetlana'} id={5}/>
             </div>
 
             <div className={styles.dialogs__messages}>
-                <div className={styles.dialogs__item}>Hi</div>
-                <div className={styles.dialogs__item}>How are you?</div>
-                <div className={styles.dialogs__item}>YOoo</div>
-                <div className={styles.dialogs__item}>See you soon</div>
+                <DialogItem message={'Hi'}/>
+                <DialogItem message={'How are you'}/>
+                <DialogItem message={'Good'}/>
+                <DialogItem message={'I am study at quarter to 10 p.m.'}/>
+                <DialogItem message={'Great work'}/>
+
             </div>
 
         </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css'
+import logo from '../../img/header.svg'
 
 export const Header = () => {
     return (
         <header className={styles.header}>
                 <div className={styles.header__container}>
-                    Header Logo
+                    <img className={styles.header__logo_img} src={logo} alt="headerLogo"/>
+                    <p className={styles.header__title}>Incubator network</p>
                 </div>
         </header>
     )
